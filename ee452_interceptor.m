31386d9499c3c89ef96e8fceade7faba6ea5c907
LeVoir <Thomas.LeVoir@mines.sdsmt.edu>
2019-12-11 18:49:49 -0700

    Modified the control law to track the direction towards the target.

--- ee452_interceptor.m
+++ ee452_interceptor.m
@@ -5,9 +5,6 @@ clc; clear all; close all;
 % ... or as a position in 2d space to try and hit during flight
 
 % target location in cartesian coordinates
-global xt
-global yt
-
 xt = 10;
 yt = 40;
 
@@ -16,16 +13,10 @@ global t_hit
 t_hit = 10;
 
 % missile location in cartesian coordinates
-global xm
-global ym
-
 xm = 0;
 ym = 0;
 
-global vd % desired forward velocity
-global wd % desired angular position
-
-vd = 10;
+vd = 5;
 wd = 2*pi/3;
 
 %% Iterative Solver to replace ode45
@@ -37,25 +28,30 @@ A = [0 0 0 ;
 B = [1 0 ;
      0 0 ;
      0 1];
-X = [-vd, pi/2-wd, 0]; % initial conditions
+X = [vd, 0, 0]; % initial conditions
 x = X.';
 dt = 0.05; % timestep size
 end_time = 10;
 for n = dt:dt:end_time
-% define control law
-u = zeros(2,1);
-r = [vd; % desired forward velocity
-     wd; % desired angular position
-     0]; % desired angular velocity
-p = [-1 -2 -3];
-k = place(A,B,p);
-u = -k*x;
-
-% define results
-dx = A*x + B*u;
-x = x + dx*dt;
-X = [X; x.'];
-
+    % define control law
+    %vd = 10;%sqrt((xt-xm)^2+(yt-ym)^2);
+    wd = atan2(yt-ym, xt-xm);
+    r = [vd; % desired forward velocity
+        wd; % desired angular position
+        0]; % desired angular velocity
+    p = [-1 -2 -3];
+    k = place(A,B,p);
+    xh = x-r;
+    xh(2) = wrapToPi(xh(2));
+    
+    % define results
+    dx = (A-B*k)*(xh);
+    x = x + dx*dt;
+    X = [X; x.'];
+    
+    xm = xm+x(1)*cos(x(2))*dt;
+    ym = ym+x(1)*sin(x(2))*dt;
+    
 end
 tspan = 0:dt:end_time;
 
@@ -64,55 +60,47 @@ plot(tspan,X)
 legend('x1 - forward velocity','x2 - angular position','x3 - angular velocity')
 grid on;
 
-%% plot dynamics
-t = 20; % how far to take data from the IC
-ts = 500; % sample size
-tspan = linspace(0,t,ts); % discretized time values to sample
-tol = 1e-10; % universal tolerance
-options = odeset('RelTol',tol,'AbsTol',[tol tol tol]); % tolerances
-init_cond = [-vd, pi/2-wd, 0]; % initial conditions
-[T,X] = ode45(@interceptor45,tspan,init_cond,options);
-figure(2)
-plot(tspan,X)
-legend('x1 - forward velocity','x2 - angular position','x3 - angular velocity')
-grid on;
 
 %% define initial position and orientation for iteration
 
-x = 0; % initial x position
-y = 0; % initial y position
+xm = 0; % initial x position
+ym = 0; % initial y position
 phi = 0; % initial angle added onto desired angle
 
 %% iterate ode45 results through time
-for n = 2:1:ts
+for n = 2:1:size(tspan')
     % X(n,1) is forward velocity
     % X(n,2) is angular position
     % X(n,3) is angular velocity
     
-    % get change in time
-    dt = T(n) - T(n-1);
-    
     % update angular orientation
-    phi = X(n,2) + wd;
+    phi = X(n,2);
     
     % update position
-    x = x + (X(n,1)+vd)*cos(phi)*dt;
-    y = y + (X(n,1)+vd)*sin(phi)*dt;
+    xm = xm + X(n,1)*cos(phi)*dt;
+    ym = ym + X(n,1)*sin(phi)*dt;
     
     % Calculate the trajectory to the target
-    distance = sqrt((xt-x)^2+(yt-y)^2);
-    direction = atan2(yt-y, xt-x);
+    distance = sqrt((xt-xm)^2+(yt-ym)^2);
+    direction = atan2(yt-ym, xt-xm);
     
     % draw the interceptor on the x-y plane
     figure(3)
-    plot(x,y,'ko')
+    plot(xm,ym,'ko')
     w = 50; % window size
     axis([-w w 0 w])
+    
+    % Draw the distance and direction
+    dist_text = sprintf('%f',distance);
+    dir_text = sprintf('%f', direction);
+    text(xm+5, ym-5, dist_text);
+    text(xm+5, ym-10, dir_text);
+    
     grid on
     hold on
     
     % draw the target
-    figure(3)
+    %figure(3)
     plot(xt,yt,'ro')
     
     % draw the desired time to impact
@@ -120,42 +108,9 @@ for n = 2:1:ts
     text(1.1*xt,1.1*yt,timer)
     t_hit = t_hit - dt;
     
-    % Draw the distance and direction
-    dist_text = sprintf('%f',distance);
-    dir_text = sprintf('%f', direction);
-    text(x+5, y-5, dist_text);
-    text(x+5, y-10, dir_text);
-    
     hold off
     
     % capture frame for movie
     F(n-1) = getframe;
     % movie(F) % to play movie
-end
-
-%% functions
-function dx = interceptor45(~,x)
-
-global vd
-global wd
-
-% set up dynamics for interceptor
-A = [0 0 0 ;
-     0 0 1 ;
-     0 0 0];
-B = [1 0 ;
-     0 0 ;
-     0 1];
-
-% define control law
-u = zeros(2,1);
-r = [vd; % desired forward velocity
-     wd; % desired angular position
-     0]; % desired angular velocity
-p = [-1 -2 -3];
-k = place(A,B,p);
-u = -k*x;
-
-% define results
-dx = A*(x-r) + B*u + A*r;
 end
